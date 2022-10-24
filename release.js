21bb34eb598d5ad66bfa722111e27809bbeb73df
Mileydis Suárez <mileydis.suarez@innocv.com>
2022-10-24 12:59:07 +0200

    ci: update changelog authomaticlaly

--- release.js
+++ release.js
@@ -4,6 +4,17 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/git"
+        [
+            "@semantic-release/changelog",
+            {
+              "changelogFile": "CHANGELOG.md"
+            }
+          ],
+          [
+            "@semantic-release/git",
+            {
+              "assets": ["CHANGELOG.md"]
+            }
+          ]
       ] 
   }
