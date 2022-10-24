af9e14144f9fcab3f8db964859fafb88736651f1
Mileydis Suárez <mileydis.suarez@innocv.com>
2022-10-24 16:33:18 +0200

    feat(core): new changelog generator

--- release.js
+++ release.js
@@ -1,10 +1,23 @@
 module.exports = {
-    branches: "develop",
+    branches: "master",
     repositoryUrl: "https://github.com/milesuarez/test-gh-actions/",
     plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/changelog",
-        "@semantic-release/git"
-    ]
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+          '@semantic-release/changelog',
+          {
+            changelogFile: 'CHANGELOG.md'
+          }
+        ],
+        '@semantic-release/npm',
+        '@semantic-release/github',
+        [
+          '@semantic-release/git',
+          {
+            assets: ['CHANGELOG.md', 'dist/**'],
+            message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+          }
+        ]
+      ]
   }
